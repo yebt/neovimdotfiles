6380708e260b4e33223fa654ca38490ea9758946
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2022-01-28 16:02:34 -0500

    Add syntax highlight from treesiter

--- loaders.lua
+++ loaders.lua
@@ -1,9 +1,10 @@
 require'nvim-treesitter.configs'.setup ({
     ensure_installed = "all",
-    -- highlight = {
-    --     enable = true,
-    -- --     additional_vim_regex_highlighting= {'vim'}
-    -- },
+    highlight = {
+        enable = true,
+        disable = { "html" },
+        additional_vim_regex_highlighting= {'vim'}
+    },
     -- indent = {
     --     enable = true
     -- },
